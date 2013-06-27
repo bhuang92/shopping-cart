e2dfe2d84167faa1b07ed67e795189da106acc25
Basil Huang <bhuang@u.northwestern.edu>
2013-06-27 09:18:24 -0500

    Initial commit for basic rails shopping app

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd1f23687f3cb888485ceef45bc6acdc49c9b5060b944cf9407652f9f7ed63358d4f427e1494087dd31d7ffea461f5e91814741b6ab3489225602ce27c6edeb34'
